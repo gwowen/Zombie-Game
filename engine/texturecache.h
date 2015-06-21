9ca5d5864eab830fe8daab0eaacc5c26a1762a9c
Gareth <gareth.w.owen@gmail.com>
2015-06-22 00:46:36 +0100

    Unfortunately the texturecache stuff didn't get saved before the commit...

--- engine/texturecache.h
+++ engine/texturecache.h
@@ -1,1 +1,21 @@
-#ifndef 
+#ifndef TEXTURECACHE_H
+#define TEXTURECACHE_H
+
+#include "gltexture.h"
+
+namespace Engine {
+
+  //this caches the textures so multiple sprites can use the same textures
+  class TextureCache
+  {
+  public:
+    TextureCache();
+    ~TextureCache();
+
+    GLTexture getTexture(std::string texturePath);
+
+  private:
+    std::map<std::string, GLTexture> _textureMap;
+  };
+
+}
