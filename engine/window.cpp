dd5264788376d8e7802c7aefe5b842839edcf5c9
Gareth <gareth.w.owen@gmail.com>
2015-06-26 00:44:16 +0100

    Finished engine coee

--- engine/window.cpp
+++ engine/window.cpp
@@ -43,6 +43,8 @@ namespace Engine {
 
       glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 
+      SDL_GL_SetSwapInterval(0);
+
       glEnable(GL_BLEND);
       glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
