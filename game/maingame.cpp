3b1546ce702b1140cf5b01ede0999da1f2ec6b97
Gareth <gareth.w.owen@gmail.com>
2015-07-06 21:18:05 +0100

    Added in exit state so can quit window

--- game/maingame.cpp
+++ game/maingame.cpp
@@ -72,7 +72,7 @@ void MainGame::processInput() {
   while(SDL_PollEvent(&event)) {
     switch(event.type) {
       case SDL_QUIT:
-
+          _gameState = GameState::EXIT; 
         break;
 
       case SDL_MOUSEMOTION:
