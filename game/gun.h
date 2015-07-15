d2cbd3e310b81ee649a96de7407f60354e929d8c
Gareth <gareth.w.owen@gmail.com>
2015-07-15 23:38:50 +0100

    Started work on adding weapons, inter-agent collisions

--- game/gun.h
+++ game/gun.h
@@ -7,9 +7,17 @@
 
 #include  "bullet.h"
 
-class Grid;
 
 class Gun {
+public:
+  Gun(std::string name, int fireRate, int bulletsPerShot, float spread, float bulletDamage, float bulletSpeed);
+  ~Gun();
+
+  void update(bool isMouseDown, const glm::vec2& position, const glm::vec2& direction, std::vector<Bullet>& bullets);
+
+private:
+
+  void fire(const glm::vec2& direction, const glm::vec2& position, std::vector<Bullet>& bullets);
 
 };
 
